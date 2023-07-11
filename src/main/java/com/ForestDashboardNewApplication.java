565a2956666d23f196d6f09834592de671a67b07
flyingfeng <2917268843@qq.com>
2023-07-11 08:55:27 +0800

    init

---
+++ src/main/java/com/ForestDashboardNewApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestDashboardNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestDashboardNewApplication.class, args);
+    }
+
+}
